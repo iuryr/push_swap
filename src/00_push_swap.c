c905d1ea810c05bb37deec0e7cf7fb40dd050cc7
iuryr <iuryroberto@gmail.com>
2023-11-14 15:12:27 -0300

    (feat) is_int check if a string is a _proper_ int, disregarding int range

--- src/00_push_swap.c
+++ src/00_push_swap.c
@@ -6,38 +6,39 @@
 /*   By: iusantos <iusantos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 11:27:41 by iusantos          #+#    #+#             */
-/*   Updated: 2023/11/13 13:35:35 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/11/14 15:10:25 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
+//returns 1 if str is a "proper" int, disregarding range. 0 otherwise
 int	is_int(char *str)
 {
-	while (*str == ' ')
+	if (!*str)
+		return (0);
+	if (*str == '-' || *str == '+')
 		str++;
-	if (*str == '+' || *str == '-')
-		str++;
-	while (*str == '0')
-		str++;
-	while(*str != '\0')
+	while (*str)
 	{
 		if (ft_isdigit(*str) == 0)
 			return (0);
-		str++;
+		else
+			str++;
 	}
 	return (1);
 }
 
-// int main(int argc, char *argv[])
-// {
-// 	size_t	i;
-//
-// 	if (argc == 1)
-// 		exit(1);
-// 	i = 1;
-// 	while(i < argc)
-// 	{
-// 		i++;
-// 	}
-// }
+int main(int argc, char *argv[])
+{
+	int	i;
+
+	if (argc == 1)
+		exit(1);
+	i = 1;
+	while(i < argc)
+	{
+		ft_printf("%s argv[%d]: %d\n", argv[i], i, is_int(argv[i]));
+		i++;
+	}
+}
