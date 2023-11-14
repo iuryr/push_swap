4217a5fa4f010fb644b029706c8aa692b872a448
iuryr <iuryroberto@gmail.com>
2023-11-14 15:47:55 -0300

    (feat) is_in_range check if a long long number is inside 4-byte int range

--- src/00_push_swap.c
+++ src/00_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: iusantos <iusantos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 11:27:41 by iusantos          #+#    #+#             */
-/*   Updated: 2023/11/14 15:10:25 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/11/14 15:34:42 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,15 @@ int	is_int(char *str)
 	return (1);
 }
 
+//returns 0 if out of int range
+int is_in_range(long long nbr)
+{
+	if (nbr < INT_MIN || nbr > INT_MAX)
+		return (0);
+	else
+		return (1);
+}
+
 int main(int argc, char *argv[])
 {
 	int	i;
@@ -38,7 +47,8 @@ int main(int argc, char *argv[])
 	i = 1;
 	while(i < argc)
 	{
-		ft_printf("%s argv[%d]: %d\n", argv[i], i, is_int(argv[i]));
+		ft_printf("%s argv[%d]: is_int %d\n", argv[i], i, is_int(argv[i]));
+		ft_printf("%s argv[%d]: is_in_range %d\n", argv[i], i, is_in_range(ft_atoll(argv[i])));
 		i++;
 	}
 }
