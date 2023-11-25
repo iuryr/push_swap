74d19cd6ddc509780b544b8dcf9c8bf5e0cea55e
iury <iuryroberto@gmail.com>
2023-11-25 19:43:57 -0300

    (feat) apply_rrarrb

--- src/03_motions.c
+++ src/03_motions.c
@@ -127,3 +127,30 @@ int	apply_rarb(t_stack *stack_a, t_stack *stack_b, int nbr, char s)
 	}
 	return (-1);
 }
+
+int	apply_rrarrb(t_stack *stack_a, t_stack *stack_b, int nbr, char s)
+{
+	if (s == 'a')
+	{
+		while((*stack_a->head)->number != nbr && get_insert_index(stack_b, nbr))
+			rrr(stack_a, stack_b);
+		while((*stack_a->head)->number != nbr)
+			rrot(stack_a->head, 0);
+		while(get_insert_index(stack_b, nbr) > 0)
+			rrot(stack_b->head, 1);
+		push(stack_a->head, stack_b->head);
+		ft_printf("pb\n");
+	}
+	else
+	{
+		while((*stack_b->head)->number != nbr && get_insert_index(stack_a, nbr) > 0)
+			rrr(stack_a, stack_b);
+		while((*stack_b->head)->number != nbr)
+			rrot(stack_b->head, 1);
+		while (get_insert_index(stack_a, nbr))
+			rrot(stack_a->head, 0);
+		push(stack_b->head, stack_a->head);
+		ft_printf("pa\n");
+	}
+	return (-1);
+}
